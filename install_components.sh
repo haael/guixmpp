6e022b12915413328b4e087ad176a129e43e3ca5
Guaz <guaz@gitlab.com>
2018-10-19 23:28:51 +0200

    install edit: build cairo symlink

--- install_components.sh
+++ install_components.sh
@@ -4,4 +4,5 @@ apt-get install python3-cairo* libcairo2-dev libjpeg-dev libgif-dev
 apt-get install -y --no-install-recommends python3-dev python3-cffi libcairo2 libpangocairo-1.0.0 libffi-dev
 
 pip install cssselect2
-pip install defusedxml 
+pip install defusedxml
+ln -s ./submodules/pycairo/build/lib.linux-x86_64-3.5/cairo/ ./cairo
