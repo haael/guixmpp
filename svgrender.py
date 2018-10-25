1dcc0cecc36f6483dd7ab4f70e5fd56657256f6c
haael <bartosz@haael.com.pl>
2018-10-25 13:06:12 +0100

    SVG widget.

--- svgrender.py
+++ svgrender.py
@@ -2,49 +2,112 @@
 #-*- coding: utf-8 -*-
 
 
+import gi	
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk as gtk
+
 import cairo
-import cairosvg
+
 import cairosvg.surface
+import cairosvg.parser
 
 
-class SVGRender:
-	def __init__(self, path):
-		self.path = path
-		self.rendered_svg_surface = None
-		self.nodes_under_pointer = None
-		
-		parent = self
-		
-		class Surface(cairosvg.surface.Surface):
-			def create_recording_surface(self, output, width, height):
-				surface = cairo.RecordingSurface(cairo.CONTENT_COLOR, (0, 0, width, height))
-				parent.rendered_svg_surface = surface
-				context = cairo.Context(surface)
-				parent.background(context)
-				return surface
-			
-			surface_class = create_recording_surface
-			
-			def finish(self):
-				assert parent.rendered_svg_surface is self.cairo
-				try:
-					parent.nodes_under_pointer = self.hover_nodes
-				except AttributeError:
-					parent.nodes_under_pointer = []
-		
-		self.Surface = Surface
+
+class SVGRender(cairosvg.surface.Surface):
+	def create_recording_surface(self, output, width, height):
+		surface = cairo.RecordingSurface(cairo.CONTENT_COLOR, (0, 0, width, height))
+		self.rendered_svg_surface = surface
+		context = cairo.Context(surface)
+		self.background(context)
+		return surface
 	
+	surface_class = create_recording_surface
+	
+	def finish(self):
+		if self.rendered_svg_surface is not self.cairo:
+			self.rendered_svg_surface = self.cairo
+		
+		try:
+			self.nodes_under_pointer = self.hover_nodes
+		except AttributeError:
+			self.nodes_under_pointer = []
+
 	def background(self, context):
 		context.set_source_rgb(1, 1, 1)
 		context.paint()
 	
-	def render(self, width, height):
-		self.Surface.convert(url=self.path, dpi=72, parent_width=width, parent_height=height)
-		return self.rendered_svg_surface
+	@classmethod
+	def render(cls, tree, width, height):
+		instance = cls(tree=tree, output=None, dpi=72, parent_width=width, parent_height=height)
+		instance.finish()
+		return instance.rendered_svg_surface
 	
-	def pointer(self, width, height, pointer_x, pointer_y):
-		self.Surface.convert(url=self.path, dpi=72, parent_width=width, parent_height=height, mouse=(pointer_x, pointer_y))
-		return self.nodes_under_pointer, self.rendered_svg_surface
+	@classmethod
+	def pointer(cls, tree, width, height, pointer_x, pointer_y):
+		instance = cls(tree=tree, output=None, dpi=72, parent_width=width, parent_height=height, mouse=(pointer_x, pointer_y))
+		instance.finish()
+		return instance.nodes_under_pointer, instance.rendered_svg_surface
+
+
+
+class SVGWidget(gtk.DrawingArea):	
+	EMPTY_SVG = b'''<?xml version="1.0" encoding="UTF-8"?>
+		<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1 1" width="1px" height="1px">
+		</svg>
+	'''
+	
+	def __init__(self):
+		super().__init__()
+		
+		class SVGRenderBg(SVGRender):
+			canvas = self
+			
+			def background(self, context):
+				canvas = self.canvas
+				canvas_allocation = canvas.get_allocation()
+				parent = canvas.get_parent()
+				parent_allocation = parent.get_allocation()
+				style_context = parent.get_style_context()
+				gtk.render_background(style_context, context, -canvas_allocation.x, -canvas_allocation.y, parent_allocation.width, parent_allocation.height)
+				gtk.render_frame(style_context, context, -canvas_allocation.x, -canvas_allocation.y, parent_allocation.width, parent_allocation.height)
+		
+		self.SVGRenderBg = SVGRenderBg
+		
+		self.document = cairosvg.parser.Tree(bytestring=self.EMPTY_SVG)
+		
+		self.rendered_svg_surface = None
+		self.nodes_under_pointer = []
+		
+		self.connect('configure-event', self.handle_configure_event)
+		self.connect('draw', self.handle_draw)
+		self.connect('motion-notify-event', self.handle_motion_notify_event)
+		
+		self.add_events(gdk.EventMask.POINTER_MOTION_MASK)
+	
+	def load_url(self, url):
+		self.tree = cairosvg.parser.Tree(url=url)
+		self.queue_draw()
+	
+	def handle_configure_event(self, drawingarea, event):
+		rect = self.get_allocation()
+		self.rendered_svg_surface = self.SVGRenderBg.render(self.document, rect.width, rect.height)
+	
+	def handle_draw(self, drawingarea, context):
+		if self.rendered_svg_surface:
+			context.set_source_surface(self.rendered_svg_surface)
+		else:
+			context.set_source_rgba(1, 1, 1)
+		context.paint()
+	
+	def handle_motion_notify_event(self, drawingarea, event):
+		rect = self.get_allocation()
+		self.nodes_under_pointer, self.rendered_svg_surface = self.SVGRenderBg.pointer(self.document, rect.width, rect.height, event.x, event.y)
+		if __debug__:
+			if self.nodes_under_pointer:
+				print(int(event.x), int(event.y), ', '.join([''.join([node.tag, ('#' + node['id'] if ('id' in node) else '')]) for node in self.nodes_under_pointer]))
+		#canvas.queue_draw()
+
+
 
 
 if __name__ == '__main__':
@@ -66,47 +129,10 @@ if __name__ == '__main__':
 	window = gtk.Window(gtk.WindowType.TOPLEVEL)
 	window.set_name('main_window')
 	
-	canvas = gtk.DrawingArea()
+	svgwidget = SVGWidget()
+	svgwidget.load_url('gfx/BYR_color_wheel.svg')
+	window.add(svgwidget)
 	
-	class SVGRenderBg(SVGRender):
-		def background(self, context):
-			canvas_allocation = canvas.get_allocation()
-			parent = canvas.get_parent()
-			parent_allocation = parent.get_allocation()
-			style_context = parent.get_style_context()
-			gtk.render_background(style_context, context, -canvas_allocation.x, -canvas_allocation.y, parent_allocation.width, parent_allocation.height)
-			gtk.render_frame(style_context, context, -canvas_allocation.x, -canvas_allocation.y, parent_allocation.width, parent_allocation.height)
-	
-	svgobject = SVGRenderBg('gfx/Steering_wheel.svg')
-	
-	rendered_svg_surface = None
-	
-	def configure_event(canvas, event):
-		global rendered_svg_surface
-		rect = canvas.get_allocation()
-		rendered_svg_surface = svgobject.render(rect.width, rect.height)
-	canvas.connect('configure-event', configure_event)
-	
-	def draw(canvas, context):
-		if rendered_svg_surface:
-			context.set_source_surface(rendered_svg_surface)
-		else:
-			context.set_source_rgba(1, 1, 1)
-		context.paint()
-	canvas.connect('draw', draw)
-	
-	def motion_notify_event(canvas, event):
-		global rendered_svg_surface
-		rect = canvas.get_allocation()
-		nodes_under_pointer, rendered_svg_surface = svgobject.pointer(rect.width, rect.height, event.x, event.y)
-		if __debug__:
-			if nodes_under_pointer:
-				print(event.x, event.y, ', '.join([''.join([node.tag, ('#' + node['id'] if ('id' in node) else '')]) for node in nodes_under_pointer]))
-		canvas.queue_draw()
-	canvas.connect('motion-notify-event', motion_notify_event)
-	
-	canvas.add_events(gdk.EventMask.POINTER_MOTION_MASK)
-	window.add(canvas)
 	window.show_all()
 	
 	mainloop = gobject.MainLoop()
