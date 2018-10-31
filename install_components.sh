ef0ba6b730b59c352aedede5f2b5ced56d1b8958
Guaz <maciej.pawlowski@mixbox.pl>
2018-10-31 18:25:56 +0100

    commit

--- install_components.sh
+++ install_components.sh
@@ -5,14 +5,3 @@ apt-get install -y --no-install-recommends python3-dev python3-cffi libcairo2 li
 
 pip install cssselect2
 pip install defusedxml
-
-pyver=$(python3 --version) | cut -c8-10
-(
- cd cairo
- rm -rf build
- mkdir build
- ./setup.sh build
-)
-ln -sf ./submodules/pycairo/build/lib.linux-x86_64-${pyver}/cairo 
-ln -sf ./submodules/CairoSVG/cairosvg/
-ln -sf ./submodules/slixmpp/build/lib/slixmpp/
