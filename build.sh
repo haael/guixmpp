c3c02443a97c350efbdc4e0860d22e614b7aaa03
Guaz <maciej.pawlowski@mixbox.pl>
2018-10-31 18:31:42 +0100

    commit

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ pyver="$(python3 --version | cut -c8-10)"
  cd submodules/pycairo
  rm -rf build
  mkdir build
- python3 setup.py build
+ python3 ./setup.py build
 )
 ln -sf ./submodules/pycairo/build/lib.linux-x86_64-${pyver}/cairo
 ln -sf ./submodules/CairoSVG/cairosvg
