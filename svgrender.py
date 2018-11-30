848cb4e0523c8ef368930ad2ce73c5481f0f15aa
Guaz <maciej.pawlowski@mixbox.pl>
2018-11-30 20:11:53 +0100

    mbp/assert for any click appear for clicked.

--- svgrender.py
+++ svgrender.py
@@ -421,6 +421,7 @@ class SVGWidget(gtk.DrawingArea):
 
 			elif handler == "clicked":
 				assert all(_ms_ev.type_ == "click" for _ms_ev in self.emitted_dom_events), "For `clicked`, only event of type `click` should be emitted."
+				assert any(_ms_ev.type_ == "click" for _ms_ev in self.emitted_dom_events) if nup else True, "For `clicked`, any event of type `click` should be emitted."
 
 			self.emitted_dom_events.clear()
 
